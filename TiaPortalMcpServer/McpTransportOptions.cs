dc449c24b5ec4d3d748b48596424ca367937e55c
Chew Chee Wai <chwai87@gmail.com>
2026-02-16 09:16:52 +0800

    delete: remove McpTransportOptions class
    This commit deletes the McpTransportOptions class, which is no longer
    needed in the project. This helps to clean up the codebase and
    eliminate unused components.

--- TiaPortalMcpServer/McpTransportOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace TiaPortalMcpServer
-{
-    public class McpTransportOptions
-    {
-        public string Host { get; set; } = "127.0.0.1";
-        public int Port { get; set; } = 3000;
-    }
-}
